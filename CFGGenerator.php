c834a0a9a80824f68fd19a4686b701675571c103
Derrick Smith <derricksmith01@msn.com>
2018-11-13 16:53:03 -0700

    Update CFGGenerator.php

--- CFGGenerator.php
+++ CFGGenerator.php
@@ -859,10 +859,7 @@ class nodeFunctionVisitor extends PhpParser\NodeVisitorAbstract{
     
     public function leaveNode(Node $node){
         //处理过程间代码，即调用的方法定义中的源码
-        if(($node->getType() == 'Expr_FuncCall' ||
-            $node->getType() == 'Expr_MethodCall' ||
-            $node->getType() == 'Expr_StaticCall' ||
-            $node->getType() == "Expr_Isset")){
+        
             $this->cfgGen->functionHandler($node, $this->block, $this->fileSummary);
         }
     }
